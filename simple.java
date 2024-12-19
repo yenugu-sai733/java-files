01f92c625a713d593192cba1c1d17b23a7c55386
yenugu-sai733 <stylishsai733@gmail.com>
2024-12-19 12:56:50 +0530

    Add files via upload
    updated

---
+++ simple.java
@@ -0,0 +1,13 @@
+class  simple
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Starting!");
+		System.out.println("First!");
+		System.out.println("second!");
+		System.out.println("Third!");
+		System.out.println("fourth!");
+		System.out.println("end of  program!");
+	}
+}
